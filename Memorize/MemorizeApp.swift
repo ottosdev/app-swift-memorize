214524ced4182fdeb7fe36be31ca649396ca541e
Otto Souza <ottosouza.dev@gmail.com>
2023-06-21 20:38:40 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Otto Neto on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
